ff60b69599eb9e2323e2376f4eeacab5a5332753
violettayun <violetta.yun13@gmail.com>
2025-04-11 09:32:58 +0500

    MyList

---
+++ src/MyList.java
@@ -0,0 +1,20 @@
+public interface MyList<T> {
+    void add(T item);
+    void set(int index, T item);
+    void add(int index, T item);
+    void addFirst(T item);
+    void addLast(T item);
+    T get(int index);
+    T getFirst();
+    T getLast();
+    void remove(int index);
+    void removeFirst();
+    void removeLast();
+    void sort();
+    int index0f(Object object);
+    int lastIndex0f(Object object);
+    boolean exists(Object object);
+    Object[] toArray();
+    void clear();
+    int size();
+}
